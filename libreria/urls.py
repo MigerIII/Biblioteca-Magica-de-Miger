c3dc9d05eda527adb7aee9e1d382e16ac209e3a2
MigerIII <mige.galvez@duocuc.cl>
2022-07-15 07:49:05 -0400

    crud

--- libreria/urls.py
+++ libreria/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
+
+
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
@@ -10,4 +14,4 @@ urlpatterns = [
     path('libros/editar',views.editar, name='editar'),
    
     
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- libreria/views.py
+++ libreria/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Libro
+from.forms import LibroForm
+
+
 # Create your views here.
 def inicio(request):
     return render(request,'inicio.html')
@@ -9,10 +13,12 @@ def nosotros(request):
 
 
 def libros(request):
-    return render(request,'libros/index.html')
+    libros = Libro.objects.all()
+    return render(request, 'libros/index.html', {'libros':libros})
 
 def crear(request):
-    return render(request, 'libros/crear.html')
+    formulario = LibroForm(request.POST or None)
+    return render(request, 'libros/crear.html', {'formulario': formulario})
 
 def editar(request):
     return render(request, 'libros/editar.html')  
