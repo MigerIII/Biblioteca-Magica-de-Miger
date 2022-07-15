18a28f0ec992dcc25090feaace972aa87d5cd8d9
MigerIII <mige.galvez@duocuc.cl>
2022-07-15 08:19:39 -0400

    Boton Borrar terminado

--- libreria/views.py
+++ libreria/views.py
@@ -22,8 +22,15 @@ def crear(request):
         formulario.save()
         return redirect('libros')
 
-
     return render(request, 'libros/crear.html', {'formulario': formulario})
 
 def editar(request):
     return render(request, 'libros/editar.html')  
+
+def eliminar(request, id):
+    libro = Libro.objects.get(id=id)
+    libro.delete
+    return redirect('libros')
+
+    
+        
