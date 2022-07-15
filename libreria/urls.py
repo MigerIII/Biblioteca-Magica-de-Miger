3a11db2e0d5deba1628c9b8428b942004bcfbb1c
MigerIII <mige.galvez@duocuc.cl>
2022-07-15 06:01:28 -0400

    botones crear y editar

--- libreria/urls.py
+++ libreria/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('inicio', views.inicio, name='inicio'),
     path('nosotros', views.nosotros, name='nosotros'),
     path('libros',views.libros, name='libros'),
-    path('libros/crear',views.crear, name='crear')
+    path('libros/crear',views.crear, name='crear'),
+    path('libros/editar',views.editar, name='editar')
+    
 ]
--- libreria/views.py
+++ libreria/views.py
@@ -3,10 +3,16 @@ from django.http import HttpResponse
 # Create your views here.
 def inicio(request):
     return render(request,'inicio.html')
+
 def nosotros(request):
     return render(request,'nosotros.html')
 
+
 def libros(request):
     return render(request,'libros/index.html')
+
 def crear(request):
-    return render(request, 'libros/crear.html')    
+    return render(request, 'libros/crear.html')
+
+def editar(request):
+    return render(request, 'libros/editar.html')        
