7ff6a46851b0519b10d1fbb89c09e8f2b157ea67
MigerIII <mige.galvez@duocuc.cl>
2022-07-15 07:10:33 -0400

    admin puede acceder a la app libreria vamoshile

--- libreria/models.py
+++ libreria/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class  Libro(models.Model):
+    id=models.AutoField(primary_key=True)
+    titulo=models.CharField(max_length=100, verbose_name='Título')
+    imagen = models.ImageField(upload_to='imagenes/',verbose_name='Imagen', null=True)
+    descripcion = models.TextField(null=True,verbose_name='Descripción')
