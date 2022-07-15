ca2f65ce36383a855fa7c3eb42e64292daaa3eda
MigerIII <mige.galvez@duocuc.cl>
2022-07-15 05:31:55 -0400

    crear libros boton

--- libreria/views.py
+++ libreria/views.py
@@ -5,5 +5,8 @@ def inicio(request):
     return render(request,'inicio.html')
 def nosotros(request):
     return render(request,'nosotros.html')
+
 def libros(request):
     return render(request,'libros/index.html')
+def crear(request):
+    return render(request, 'libros/crear.html')    
